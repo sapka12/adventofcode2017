fbe1bc7124617ae9645d5cf216d0f993bd340dfc
Arnold Farkas <farkasarnold1981@gmail.com>
2017-12-01 11:40:45 +0100

    refactor

--- day1/captcha.py
+++ day1/captcha.py
@@ -7,37 +7,60 @@ def eq(n0, n1):
 	return int(n0) == int(n1)
 
 
-def nthPair(char_list, n0):
+def nth_pair(char_list, n0, find_n_plus1_idx):
 	size = len(char_list)
 	n0 = int(n0) % size
-	n1 = (n0 + 1) % size
+	n1 = find_n_plus1_idx(size, n0)
 	return (char_list[n0], char_list[n1])
 
-def sumOfPair(chars, n):
-	(a, b) = nthPair(chars, n)
+
+def nth_sum_of_pair(chars, n, strategy):
+	(a, b) = nth_pair(chars, n, strategy)
 	if (a == b):
 		return int(a)
 	else:
 		return 0
 
 
-def captcha(input_str):
+def captcha(input_str, strategy):
 	chars = list(input_str)
-	sums = [ sumOfPair(chars, idx) for idx, digit in enumerate(chars)]
+	sums = [ nth_sum_of_pair(chars, idx, strategy) for idx, digit in enumerate(chars)]
 	return sum(sums)
 
 
+def run(inputs, strategy):
+	for input_captcha in inputs:
+		print(input_captcha + " : " + str(captcha(input_captcha, strategy)))
+
+
 def main():	
-	inputs = [
+	
+	def find_n_plus1_next(size, n):
+		return (int(n) + 1) % size
+
+
+	def find_n_plus1_halfway(size, n):
+		return (n + int(size / 2)) % size
+	
+	
+	run([
 		"1122", 
 		"1111", 
 		"1234", 
 		"91212129",
 		content('input.txt')
-	]
-
-	for input_captcha in inputs:
-		print(input_captcha + " : " + str(captcha(input_captcha)))
+	], find_n_plus1_next)
+	
+	run([
+		"1212", 
+		"1221", 
+		"123425", 
+		"123123",
+		"12131415",
+		content('input.txt')
+	], find_n_plus1_halfway)
 
 
 main()
+
+
