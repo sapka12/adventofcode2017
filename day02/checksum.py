2e68af889e15ada03a62f26aac0b384a158fd4c3
Arnold Farkas <farkasarnold1981@gmail.com>
2017-12-02 22:35:54 +0100

    refactor

--- day02/checksum.py
+++ day02/checksum.py
@@ -14,26 +14,26 @@ def check_sum(data):
 	
 	return sum([ row_sum(row)  for row in data])
 
-#print(check_sum(read_tab_file()))
+print(check_sum(read_tab_file()))
 
 
 def check_sum2(data):
 	
 	def row_sum(row):
 		int_row = map(int, row)
-		row_range = range(len(row))
-		
-		for a in row_range:
-			for b in row_range:
-				if a != b and int_row[b] != 0 and int_row[a] % int_row[b] == 0:
-					aa = int_row[a]
-					bb = int_row[b]
-					return aa // bb
-	
+		row_range = range(len(int_row))
+
+		return [e for e in [
+			[
+				int_row[a] // int_row[b]
+				for b in row_range
+				if a != b and int_row[b] != 0 and int_row[a] % int_row[b] == 0
+			]
+			for a in row_range
+		] if len(e) == 1][0][0]
+
 	return sum([ row_sum(row)  for row in data])
 
-
-print(read_tab_file())
 
 print(check_sum2(read_tab_file()))
 
