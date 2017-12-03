4b3a5ba7bdeb61b68e26dc10f9d24552cf00f6e8
Arnold Farkas <farkasarnold1981@gmail.com>
2017-12-03 17:02:39 +0100

    day3 - ineffective solution

--- day03/day3.py
+++ day03/day3.py
@@ -1,24 +1,75 @@
 def maximum(arr):
     return max([max(a) for a in arr])
 
-def generate_square(x):
-	size = 2 * x + 1
-	square = [[None] * size for i in range(size) ]
-    #fill square
-	return square
+
+def next_layer(arr):
+    print(maximum(arr))
+    orig_size = len(arr)
+    size = orig_size + 2
+
+    result = [[None] * size for i in range(size)]
+
+    for x in range(orig_size):
+        for y in range(orig_size):
+            result[x + 1][y + 1] = arr[x][y]
+
+    actual_max = maximum(arr)
+    right = [actual_max + 1 + i for i in range(orig_size + 1)]
+
+    actual_max = max(right)
+    top = [actual_max + 1 + i for i in range(orig_size + 1)]
+
+    actual_max = max(top)
+    left = [actual_max + 1 + i for i in range(orig_size + 1)]
+
+    actual_max = max(left)
+    bottom = [actual_max + 1 + i for i in range(orig_size + 1)]
+
+    for i in range(len(right)):
+        result[len(result) - 1][len(result) - 2 - i] = right[i]
+
+    for i in range(len(top)):
+        result[len(result) - 2 - i][0] = top[i]
+
+    for i in range(len(left)):
+        result[0][1 + i] = left[i]
+
+    for i in range(len(bottom)):
+        result[1 + i][len(result) - 1] = bottom[i]
+
+    return result
+
+
+def generate_square_which_contains(x):
+    result = [[1]]
+
+    while maximum(result) < x:
+        result = next_layer(result)
+
+    return result
 
 def manhattan_disatance(pos1, pos2):
     (pos1x, pos1y) = pos1
     (pos2x, pos2y) = pos2
     return abs(pos1x - pos2x) + abs(pos1y - pos2y)
 
+
 def pos_of_val(arr, value):
-    for x in range(arr):
-        for y in range(arr[x]):
+    for x in range(len(arr)):
+        for y in range(len(arr[x])):
             if arr[x][y] == value:
                 return (x, y)
+
 
 def get_distance_from_1(arr, value):
     pos_1 = pos_of_val(arr, 1)
     pos_val = pos_of_val(arr, value)
     return manhattan_disatance(pos_1, pos_val)
+
+
+def manhattan_disatance_from_1(num):
+    arr = generate_square_which_contains(num)
+    return get_distance_from_1(arr, num)
+
+
+print(manhattan_disatance_from_1(361527))
--- day03/test.py
+++ day03/test.py
@@ -1,32 +1,13 @@
 import unittest
-from day3 import maximum, generate_square
+from day3 import manhattan_disatance_from_1
 
 class TestMethods(unittest.TestCase):
 
-    def test_max(self):
-        self.assertEqual(maximum([[1]]), 1)
-        self.assertEqual(maximum([[1, 2]]), 2)
-        self.assertEqual(maximum([[1, 2], [3]]), 3)
-        self.assertEqual(maximum([[1, 4, 2], [3]]), 4)
-        
-    def test_generate_square(self):
-		self.assertEqual(generate_square(0), [[1]])
-		self.assertEqual(generate_square(1), 
-		[
-			[5, 4, 3],
-			[6, 1, 2],
-			[7, 8, 9]
-		]
-		)
-		self.assertEqual(generate_square(2), 
-		[
-			[17, 16, 15, 14, 13],
-			[18,  5,  4,  3, 12],
-			[19,  6,  1,  2, 11],
-			[20,  7,  8,  9, 10],
-			[21, 22, 23, 24, 25]
-		]
-		)
+    def test_manhattan_disatance_from_1(self):
+        self.assertEqual(manhattan_disatance_from_1(1), 0)
+        self.assertEqual(manhattan_disatance_from_1(12), 3)
+        self.assertEqual(manhattan_disatance_from_1(23), 2)
+        self.assertEqual(manhattan_disatance_from_1(1024), 31)
 
 
 if __name__ == '__main__':
