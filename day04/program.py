ad8b2efe83f45e2b38a79cd73e021b039b75e8bc
Arnold Farkas <farkasarnold1981@gmail.com>
2017-12-04 16:08:41 +0100

    refactor

--- day04/program.py
+++ day04/program.py
@@ -3,37 +3,27 @@ def read_input():
         return [x.strip() for x in f.readlines()]
 
 
-def task1():
-    data = read_input()
-    return sum([1 for pw in data if is_valid(pw)])
-
-
-def is_valid_wo_anagrams(pw):
+def validate(pw, validator_fn):
     words = pw.split(" ")
 
     def count_word(word):
-        return len([w for w in words if are_anagrams(w, word)])
+        return len([w for w in words if validator_fn(w, word)])
 
     return all(count_word(word) == 1 for word in words)
-
-
-def is_valid(pw):
-    words = pw.split(" ")
-
-    def count_word(word):
-        return len([w for w in words if w == word])
-
-    return all(count_word(word) == 1 for word in words)
-
-
-def task2():
-    data = read_input()
-    return sum([1 for pw in data if is_valid_wo_anagrams(pw)])
 
 
 def are_anagrams(str0, str1):
     return sorted(list(str0)) == sorted(list(str1))
 
 
-print(task1())
-print(task2())
+def task(password_match_fn):
+    data = read_input()
+    return sum([1 for pw in data if validate(pw, password_match_fn)])
+
+
+def task1():
+    return task(lambda x, y: x == y)
+
+
+def task2():
+    return task(are_anagrams)
--- day04/test.py
+++ day04/test.py
@@ -1,18 +1,18 @@
 import unittest
-from program import is_valid, are_anagrams
+from program import are_anagrams, task1, task2
 
 class TestMethods(unittest.TestCase):
-
-    def test_is_valid(self):
-        self.assertTrue(is_valid("aa bb cc dd ee"))
-        self.assertFalse(is_valid("aa bb cc dd aa"))
-        self.assertTrue(is_valid("aa bb cc dd aaa"))
-
 
     def test_is_are_anagrams(self):
         self.assertTrue(are_anagrams("asdf", "sdfa"))
         self.assertTrue(are_anagrams("asdf", "asdf"))
         self.assertFalse(are_anagrams("asdf1", "asdf2"))
+
+    def test_task1(self):
+        self.assertEquals(task1(), 386)
+
+    def test_task2(self):
+        self.assertEquals(task2(), 208)
 
 
 if __name__ == '__main__':
