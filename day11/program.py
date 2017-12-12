531cea2ebeee8267eecd8076c54b02966ff78313
Arnold Farkas <farkasarnold1981@gmail.com>
2017-12-12 01:47:12 +0100

    day 11

--- day11/program.py
+++ day11/program.py
@@ -1,37 +1,93 @@
 from functools import reduce
 
 
+def to_vector(text):
+    if text == "n":
+        return 1, 1
+    if text == "ne":
+        return 0, 1
+    if text == "nw":
+        return 1, 0
+    if text == "s":
+        return -1, -1
+    if text == "se":
+        return -1, 0
+    if text == "sw":
+        return 0, -1
+
+
+vec = ["n", "ne", "se", "s", "sw", "nw"]
+vec2 = [to_vector(i) for i in vec]
+
+
 def invert(text):
-	if text == "n":
-		return "s"
-	if text == "s":
-		return "n"
-	if text == "ne":
-		return "sw"
-	if text == "nw":
-		return "se"
-	if text == "se":
-		return "nw"
-	if text == "sw":
-		return "ne"
+    if text == "n":
+        return "s"
+    if text == "s":
+        return "n"
+    if text == "ne":
+        return "sw"
+    if text == "nw":
+        return "se"
+    if text == "se":
+        return "nw"
+    if text == "sw":
+        return "ne"
+
 
 def remove_one_pair_of_inverts(vector):
-	for x in vector:
-		inv = invert(x)
-		if inv in vector:
-			vector.remove(x)
-			vector.remove(inv)
-			return vector
-	return vector
-			
-
-def task1(vectors):
-	res = vectors
-	for i in range(len(vectors)):
-		res = remove_one_pair_of_inverts(res)
-		
-	return len(res)
+    for x in vector:
+        inv = invert(x)
+        if inv in vector:
+            vector.remove(x)
+            vector.remove(inv)
+            return vector
+    return vector
 
 
-def task2():
-	print()
+def vector_add(a, b):
+    return a[0] + b[0], a[1] + b[1]
+
+
+def create_next_layer(layer_id, new_layer, visited):
+    print(layer_id)
+    list_of_list = [[vector_add(v, outer_elem) for v in vec2] for outer_elem in new_layer]
+    flatten = set([item for sublist in list_of_list for item in sublist])
+    new_layer = set(filter(lambda x: x not in visited, flatten))
+    visited = visited | new_layer
+    return layer_id + 1, new_layer, visited
+
+
+def end_pos(steps):
+    return reduce(vector_add, [to_vector(step) for step in steps])
+
+
+def task1(steps):
+    x, y = end_pos(steps)
+    print(x, y)
+
+    visited = set([(0, 0)])
+    new_layer = visited
+    layer = 0
+    while (x, y) not in visited:
+        layer, new_layer, visited = create_next_layer(layer, new_layer, visited)
+    return layer
+
+
+def task2(vectors):
+    all = len(vectors)
+
+    visited = set([(0, 0)])
+    new_layer = visited
+    layer = 0
+
+    for i in range(all):
+
+        print("step: ", i, (100.0 * i) / all)
+
+        path = vectors[0:i + 1]
+        x, y = end_pos(path)
+        while (x, y) not in visited:
+            layer, new_layer, visited = create_next_layer(layer, new_layer, visited)
+
+    return layer
--- day11/test.py
+++ day11/test.py
@@ -3,22 +3,27 @@ import math
 from program import task1, task2
 
 
-def read_str(input_file):
-    with open(input_file) as f:
-        return [x.strip() for x in f.readlines()][0]
-
-
 def read(input_file):
-    return [int(i) for i in read_str(input_file).split(",")]
+    with open(input_file) as f:
+        return [x.strip() for x in f.readlines()][0].split(",")
 
 
 class TestMethods(unittest.TestCase):
+
     def test_task1(self):
 		self.assertEquals(task1("ne,ne,ne".split(",")), 3)
 		self.assertEquals(task1("ne,ne,sw,sw".split(",")), 0)
 		self.assertEquals(task1("ne,ne,s,s".split(",")), 2)
 		self.assertEquals(task1("se,sw,se,sw,sw".split(",")), 3)
+		# self.assertEquals(task1(read("input.txt")), 773)
 
+    def test_task2(self):
+        self.assertEquals(task2("ne,ne,ne".split(",")), 3)
+        self.assertEquals(task2("ne,ne,sw,sw".split(",")), 2)
+        self.assertEquals(task2("ne,ne,s,s".split(",")), 2)
+        self.assertEquals(task2("se,sw,se,sw,sw".split(",")), 3)
+        self.assertEquals(task2("n,n,s,s".split(",")), 2)
+        self.assertEquals(task2(read("input.txt")), 0)
 
 if __name__ == '__main__':
     unittest.main()
