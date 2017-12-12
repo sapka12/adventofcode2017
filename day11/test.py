f144ecd385054867635e671153ba713b9f2b2e62
Arnold Farkas <farkasarnold1981@gmail.com>
2017-12-12 01:50:24 +0100

    day 11

--- day11/test.py
+++ day11/test.py
@@ -1,5 +1,4 @@
 import unittest
-import math
 from program import task1, task2
 
 
@@ -9,13 +8,12 @@ def read(input_file):
 
 
 class TestMethods(unittest.TestCase):
-
     def test_task1(self):
-		self.assertEquals(task1("ne,ne,ne".split(",")), 3)
-		self.assertEquals(task1("ne,ne,sw,sw".split(",")), 0)
-		self.assertEquals(task1("ne,ne,s,s".split(",")), 2)
-		self.assertEquals(task1("se,sw,se,sw,sw".split(",")), 3)
-		# self.assertEquals(task1(read("input.txt")), 773)
+        self.assertEquals(task1("ne,ne,ne".split(",")), 3)
+        self.assertEquals(task1("ne,ne,sw,sw".split(",")), 0)
+        self.assertEquals(task1("ne,ne,s,s".split(",")), 2)
+        self.assertEquals(task1("se,sw,se,sw,sw".split(",")), 3)
+        self.assertEquals(task1(read("input.txt")), 773)
 
     def test_task2(self):
         self.assertEquals(task2("ne,ne,ne".split(",")), 3)
@@ -23,7 +21,8 @@ class TestMethods(unittest.TestCase):
         self.assertEquals(task2("ne,ne,s,s".split(",")), 2)
         self.assertEquals(task2("se,sw,se,sw,sw".split(",")), 3)
         self.assertEquals(task2("n,n,s,s".split(",")), 2)
-        self.assertEquals(task2(read("input.txt")), 0)
+        self.assertEquals(task2(read("input.txt")), 1560)
+
 
 if __name__ == '__main__':
     unittest.main()
