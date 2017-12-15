c6ca154d2cb60cde8a556017ef30c771b8ad4245
Arnold Farkas <farkasarnold1981@gmail.com>
2017-12-15 22:00:41 +0100

    day 15 - part2

--- day15/program.py
+++ day15/program.py
@@ -1,9 +1,15 @@
+import math
+
 factor_a = 16807
 factor_b = 48271
 
 
-def one_round(seed, factor):
-    return (seed * factor) % 2147483647
+def one_round(seed, factor, divider=1):
+    r = (seed * factor) % 2147483647
+    if r % divider == 0:
+        return r
+    else:
+        return one_round(r, factor, divider)
 
 
 def to_binary(num):
@@ -14,17 +20,29 @@ def last16bits(num):
     return to_binary(num)[-16:]
 
 
+_16bit = math.pow(2, 16)
+
+
 def last_16_bits_match(num1, num2):
-    return last16bits(num1) == last16bits(num2)
+    res = num1 ^ num2
+    return res % _16bit == 0
 
 
-def task1(seed_a, seed_b, rounds):
+def go(seed_a, seed_b, rounds, divider_a, divider_b):
     counter = 0
     for a_round in range(rounds):
 
-        seed_a = one_round(seed_a, factor_a)
-        seed_b = one_round(seed_b, factor_b)
+        seed_a = one_round(seed_a, factor_a, divider_a)
+        seed_b = one_round(seed_b, factor_b, divider_b)
 
         if last_16_bits_match(seed_a, seed_b):
             counter = counter + 1
     return counter
+
+
+def task1(seed_a, seed_b, rounds):
+    return go(seed_a, seed_b, rounds, 1, 1)
+
+
+def task2(seed_a, seed_b, rounds):
+    return go(seed_a, seed_b, rounds, 4, 8)
--- day15/test.py
+++ day15/test.py
@@ -1,18 +1,40 @@
 import unittest
-from program import task1, last_16_bits_match
+from program import task1, task2, last_16_bits_match, one_round
 
 
 class TestMethods(unittest.TestCase):
+
     def test_check_last16(self):
         self.assertTrue(last_16_bits_match(245556042, 1431495498))
         self.assertFalse(last_16_bits_match(1092455, 430625591))
         self.assertFalse(last_16_bits_match(1181022009, 1233683848))
         self.assertFalse(last_16_bits_match(1744312007, 137874439))
 
-    def test_find_neighbours(self):
+    def test_task1(self):
         self.assertEqual(task1(65, 8921, 5), 1)
         self.assertEqual(task1(65, 8921, 40 * 1000 * 1000), 588)
         self.assertEqual(task1(722, 354, 40 * 1000 * 1000), 612)
+
+    def test_gen(self):
+        self.assertEqual(one_round(65, 16807, 4), 1352636452)
+        self.assertEqual(one_round(8921, 48271, 8), 1233683848)
+
+        self.assertEqual(one_round(1352636452, 16807, 4), 1992081072)
+        self.assertEqual(one_round(1233683848, 48271, 8), 862516352)
+
+        self.assertEqual(one_round(1992081072, 16807, 4), 530830436)
+        self.assertEqual(one_round(862516352, 48271, 8), 1159784568)
+
+        self.assertEqual(one_round(530830436, 16807, 4), 1980017072)
+        self.assertEqual(one_round(1159784568, 48271, 8), 1616057672)
+
+        self.assertEqual(one_round(1980017072, 16807, 4), 740335192)
+        self.assertEqual(one_round(1616057672, 48271, 8), 412269392)
+
+    def test_task2(self):
+        self.assertEqual(task2(65, 8921, 5), 0)
+        self.assertEqual(task2(65, 8921, 5 * 1000 * 1000), 309)
+        self.assertEqual(task2(722, 354, 5 * 1000 * 1000), 285)
 
 
 if __name__ == '__main__':
