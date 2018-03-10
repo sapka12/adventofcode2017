932b5b47f898bfd79f5c8a442571d00c1734500e
Arnold Farkas <farkasarnold1981@gmail.com>
2018-03-10 16:02:55 +0100

    day21 - in porgress

--- day21/program.py
+++ day21/program.py
@@ -1,6 +1,19 @@
 import numpy as np
 
 
+def to_mx(text):
+    def to_bool(x):
+        if x == "#":
+            return 1
+        else:
+            return 0
+
+    return np.vectorize(to_bool)(np.array([list(row) for row in text.split("/")]))
+
+
+start_image = to_mx(".#./..#/###")
+
+
 def read(input_file):
     with open(input_file) as f:
         return [x.strip() for x in f.readlines()]
@@ -16,25 +29,10 @@ def key(mx):
 
 
 def variations(mx):
-    return [
-        np.fliplr(mx),
-        np.flipud(mx),
-        np.rot90(mx, 1),
-        np.rot90(mx, 2),
-        np.rot90(mx, 3),
-        mx
-    ]
+    rotations = [np.rot90(mx, 1) for i in range(4)]
+    _all_variations = [[mx, np.fliplr(mx), np.flipud(mx)] for mx in rotations]
 
-
-def to_mx(text):
-
-    def to_bool(x):
-        if x == "#":
-            return 1
-        else:
-            return 0
-
-    return np.vectorize(to_bool)(np.array([list(row) for row in text.split("/")]))
+    return [item for sublist in _all_variations for item in sublist]
 
 
 for line in f:
@@ -46,4 +44,36 @@ for line in f:
         all_variations[key(_from)] = _to
 
 
-print(len(all_variations.values()))
+def change_mx(mx, _variations):
+    return _variations[key(mx)]
+
+
+def sub_mx(_mx, x_range, y_range):
+    return _mx[np.ix_(list(x_range), list(y_range))]
+
+
+def reshape_tiles(_mx, tile_size, _variations):
+    _size = len(_mx)
+    out = np.zeros(_size * _size).reshape((_size, _size))
+
+    for x in range(_size // tile_size):
+        for y in range(_size // tile_size):
+            r_x = [i + tile_size * x for i in range(tile_size)]
+            r_y = [i + tile_size * y for i in range(tile_size)]
+
+            tile = sub_mx(_mx, r_x, r_y)
+            out[x, y] = change_mx(tile, _variations)
+    return out
+
+
+def reassembly(_mx, tile_size):
+    pass
+
+
+def iterate(_mx, _variations):
+    tile_size = 2
+    if len(_mx) % tile_size == 0:
+        return reassembly(reshape_tiles(_mx, tile_size, _variations), 3)
+    else:
+        tile_size = 3
+        return reassembly(reshape_tiles(_mx, tile_size, _variations), 4)
